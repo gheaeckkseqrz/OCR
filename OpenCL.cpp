d4d2f7e4a5c926a62008cb2a0c08e3b9402104a8
Pierre Wilmot <pierre.wilmot@gmail.com>
2016-01-27 02:14:32 +0100

    Created OpenCL branch

---
+++ OpenCL.cpp
@@ -0,0 +1,6 @@
+#include "OpenCL.h"
+
+OpenCL::OpenCL()
+{
+  
+}
---
+++ OpenCL.h
@@ -0,0 +1,15 @@
+#ifndef __OPENCL_H__
+#define __OPENCL_H__
+
+#include "CL/cl.h"
+
+class OpenCL
+{
+ public:
+  OpenCL();
+
+ private:
+  
+};
+
+#endif
